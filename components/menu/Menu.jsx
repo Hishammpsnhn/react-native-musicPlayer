e533b5fd01b26070c8ab7c5e02997b46c4b2fee8
hisham <hishammpsn@gmail.com>
2023-04-12 14:09:59 +0530

    cataory menu add

--- components/menu/Menu.jsx
+++ components/menu/Menu.jsx
@@ -1,8 +1,42 @@
-import React from "react";
-import { Text } from "react-native";
+import React from 'react'
+import { FlatList, Image, ScrollView, Text, View } from 'react-native'
+import { COLORS, SIZES, icons } from '../../constants'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+const items = [
+  { icon: icons.search, text: 'somewhere' },
+  { icon: icons.search, text: 'somewhere' },
+  { icon: icons.search, text: 'somewhere' },
+  { icon: icons.search, text: 'somewhere' },
+]
 
 const Menu = () => {
-  return <Text>Menu</Text>;
-};
+  return (
+    <View
+      style={{
+        marginTop: 5,
+        backgroundColor: '#fff',
+        paddingVertical: 10,
+        paddingLeft: 20,
+      }}
+    >
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map((item, i) => (
+          <View key={i} style={{ alignItems: 'center', marginRight: 30 }}>
+            <Image
+              source={item.icon}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: 'contain',
+              }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: '900' }}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  )
+}
 
-export default Menu;
+export default Menu
